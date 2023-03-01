255818c3f57904668dd01274fb342dd493b2d927
tugi <blueqwinn@gmail.com>
2023-03-01 11:13:49 +0300

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,4 @@
 class Review < ActiveRecord::Base
   belongs_to :game
-end
-class User < ActiveRecord::Base
-  has_many (:reviews)
-  has_many :(games, through: :reviews)
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,5 @@
 class User < ActiveRecord::Base
-      def change
-          create_table :users do |t|
-            t.string :name
-            t.timestamps
-          end
-        end
-      end
-end
-
-class AddUserIdToReviews < ActiveRecord::Migration[6.1]
-    def change
-      add_column :reviews, :user_id, :integer
-    end
+    has_many :reviews
+      has_many :games, through: :reviews
   end
   
